3b59c6b94016cf5c99e7082fedcb2e35aafcd36e
KyomaHooin <kyomahooin@gmail.com>
2021-08-23 11:25:49 +0200

    魔女の直し

--- majo.sh
+++ majo.sh
@@ -4,61 +4,63 @@
 # GET
 #
 
-#wgets -O 'um-sayounara-boku-no-majo.pdf' 'https://www.japancenter.cz/upload/files/downloads/um-sayounara-boku-no-majo.pdf'
+wget -O 'um-sayounara-boku-no-majo.pdf' 'https://www.japancenter.cz/upload/files/downloads/um-sayounara-boku-no-majo.pdf'
 
 #
 # EXTRACT
 #
 
-#pdfimages um-sayounara-boku-no-majo.pdf majo
+pdfimages um-sayounara-boku-no-majo.pdf majo
 
 #
 # SPLIT
 #
 
-#INDEX=0
+mkdir split/ 2>/dev/null
 
-#mkdir split/ 2>/dev/null
+INDEX=0
 
-#for PAGE in {000..039}; do
-#	convert -crop 100%x50% "majo-$PAGE.pbm" "out_%d.pbm"
-#	mv "out_0.pbm" "split/majo-$INDEX.pbm"
-#	((INDEX++))
-#	mv "out_1.pbm" "split/majo-$INDEX.pbm"
-#	((INDEX++))
-#done
-
-#
-# ROTATE
-#
-
-# halves by 180... for left to right read..
+for PAGE in {000..039}; do
+	convert -crop 100%x50% "majo-$PAGE.pbm" "out_%d.pbm"
+	mv "out_0.pbm" "split/majo-$INDEX.pbm"
+	((INDEX++))
+	mv "out_1.pbm" "split/majo-$INDEX.pbm"
+	((INDEX++))
+done
 
 #
 # JOIN
 #
 
-#     PAGE: 0 3 2 5 4 7 6 9 8 .. 1
-#    PRINT: 0 2 | 3 5 - 4 6 | 7 9 - 8 10 | 11 13 - .. 
-
-INDEX=0
-PAGE=0
+#  PAGE: 0 3 2 5 4 7 6 9 8 .. 1
+# PRINT: 0 81 | 3 78 - 2 79 | 5 76 - 4 77 | 7 74 - .. 
 
 mkdir merge/ 2>/dev/null
 
-#mv "split/majo-1.pbm" "split/majo-81.pbm" 2>/dev/null #fix back cover..
+mv "split/majo-1.pbm" "split/majo-81.pbm" 2>/dev/null #fix back cover..
+
+FIRST=0
+LAST=81
+PAGE=0
 
 for ((i=0;i < 20;i++)) do
-	convert -append "split/majo-$INDEX.pbm" "split/majo-$(($INDEX + 2)).pbm" "merge/majo-$(printf '%03d' $PAGE).pbm";
+
+	convert -append "split/majo-$FIRST.pbm" "split/majo-$LAST.pbm" "merge/majo-$(printf '%03d' $PAGE).pbm";
 	((PAGE++))
-	convert -append "split/majo-$(($INDEX + 3)).pbm" "split/majo-$(($INDEX + 5)).pbm" "merge/majo-$(printf '%03d' $PAGE).pbm"; 
-	INDEX=$(($INDEX + 4))
+
+	mogrify -rotate 180 "split/majo-$(($FIRST + 3)).pbm"
+	mogrify -rotate 180 "split/majo-$(($LAST - 3)).pbm"
+
+	convert -append "split/majo-$(($FIRST + 3)).pbm" "split/majo-$(($LAST - 3)).pbm" "merge/majo-$(printf '%03d' $PAGE).pbm"; 
 	((PAGE++))
+
+	FIRST=$(($FIRST + 2))
+	LAST=$(($LAST - 2))
 done
 
 #
 # BACK
 #
 
-#convert -limit thread 2 "merge/majo*.pbm" fixed.pdf
+convert -limit thread 2 "merge/majo*.pbm" fixed.pdf
 
