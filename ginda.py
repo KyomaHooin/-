20769e98d24299cf109af37006e52141a90ad82e
KyomaHooin <kyomahooin@gmail.com>
2021-09-29 12:46:14 +0200

    Update.

--- ginda.py
+++ ginda.py
@@ -8,14 +8,10 @@
 #
 
 import lxml.html,sys,re
-
-#from io import BytesIO
-
 # PDF Canvas
 from reportlab.pdfgen.canvas import Canvas
 from reportlab.lib import pagesizes
 from reportlab.lib.utils import ImageReader
-
 # Kanji / Kana
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.cidfonts import UnicodeCIDFont
@@ -23,7 +19,6 @@ from reportlab.pdfbase.cidfonts import UnicodeCIDFont
 ###########################
 # VAR
 ###########################
-#
 
 FILE='sakura/ginda.html'
 DATA='sakura/ginda_data'
@@ -31,8 +26,9 @@ PDF='ginda.pdf'
 
 TITLE='銀田の事件簿'
 
-OFFSET_BOTTOM=0
-OFFSET_LEFT=0
+# 560x800
+OFFSET_TOP=560
+OFFSET_LEFT=400+400
 
 ###########################
 #INIT
@@ -51,13 +47,10 @@ pdfmetrics.registerFont(UnicodeCIDFont('HeiseiMin-W3','90ms-RKSJ-V'))# Vetical H
 
 with open(FILE,'r') as f:
 
-	# HTML parse
 	p = lxml.html.HTMLParser()
 	data = lxml.html.parse(f, p)
 
 	for el in data.xpath('./body/*'):
-		#print(el.get('class'))
-		#
 		# IMAGE
 		#
 		# logo..		ZeroDrop _pageBreakAfterAlways 
@@ -68,17 +61,11 @@ with open(FILE,'r') as f:
 		#
 		# header..		SmallHeader
 		# text bottom		AlignBottom
-		
-		# text..		FiveDrop
-		# text..		OneDrop
-		# text or break ..	ZeroDrop
 		#
-
 		# LOGO
 		#if el.get('class') == 'ZeroDrop _pageBreakAfterAlways':
 		#	img_path = DATA + '/' + re.sub('.*\/','', el.xpath('./img/@src')[0])
 		#	pdf.drawImage(ImageReader(img_path),0,0,600,600)
-
 		# HEADER
 		#if el.get('class') == 'header':
 		# TEXT
@@ -87,20 +74,45 @@ with open(FILE,'r') as f:
 			if len(el.xpath('./br')) > 0:
 				OFFSET_LEFT-=16
 			else:
-				rs = 
-				for text in el.itertext():# all text
-					# if not a ruby => print
-					# if ruby => skipt index by 2 + print furigana	
-
-				OFFSET_LEFT-=16
-		
-		#pdf.setFont('HeiseiMin-W3', 16) # 16x16
-		#pdf.drawString(50,550,"銀田の事件簿")
-
-		#pdf.setFont('HeiseiMin-W3', 8)# 8x8
-		#pdf.drawString(65,550,'ぎん')
-		#pdf.drawString(65,530,'だ')
-
+				# text
+				text = [t for t in el.itertext()]
+				# ruby
+				ruby = el.xpath('./ruby/text()')
+				# loop
+				FURI=False
+				for i in range(0,len(text)):
+					# text break
+					if OFFSET_TOP < 100:
+						OFFSET_LEFT-=2*16
+						OFFSET_TOP=560
+					# skip furigana
+					if FURI:
+						FURI=False
+						continue
+					if text[i] not in ruby:
+						pdf.setFont('HeiseiMin-W3', 16)
+						pdf.drawString(OFFSET_LEFT, OFFSET_TOP, text[i])
+						# offset
+						OFFSET_TOP-=16*len(text[i])
+					else:
+						pdf.setFont('HeiseiMin-W3', 16)
+						pdf.drawString(OFFSET_LEFT, OFFSET_TOP, text[i])
+						pdf.setFont('HeiseiMin-W3', 8)
+						pdf.drawString(OFFSET_LEFT+16, OFFSET_TOP, text[i+1])
+						# offset
+						OFFSET_TOP-=16*len(text[i])
+						# furigana
+						FURI=True
+				# line break
+				OFFSET_LEFT-=2*16
+				OFFSET_TOP=560
+			# write page
+			if OFFSET_LEFT < 20:
+				pdf.showPage()
+				OFFSET_LEFT=800
+				OFFSET_TOP=560
+				
+	
 		#pdf.drawImage(ImageReader(img),70,50,700,470)
 
 		# write page
