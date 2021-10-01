cdcded1fba58fe45412db59862e8b46af911e1c4
KyomaHooin <kyomahooin@gmail.com>
2021-10-01 12:35:39 +0200

    銀田

--- ginda.py
+++ ginda.py
@@ -2,18 +2,14 @@
 #
 # Sakura HTML to PDF
 #
-# A4 landscape: 0-
-#
 # TODO: Delete copyright data from Git history.
 # TODO: scale image to A5 page centered
-# TODO: chapter header
-#
 #
 import lxml.html,sys,re
 # PDF Canvas
 from reportlab.pdfgen.canvas import Canvas
 from reportlab.lib import pagesizes
-from reportlab.lib.utils import ImageReader
+from reportlab.lib.utils import Image,ImageReader
 # Kanji / Kana
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.cidfonts import UnicodeCIDFont
@@ -51,25 +47,25 @@ with open(FILE,'r') as f:
 	p = lxml.html.HTMLParser()
 	data = lxml.html.parse(f, p)
 
-	for el in data.xpath('./body/*'):
-	
-		# IMAGE
+	PAGE=1
 
-		#if el.get('class') in ['ZeroDrop _pageBreakAfterAlways', 'FloatCenterImage','creditsdiv']:
-		#
-		#	img_path = DATA + '/' + re.sub('.*\/','', el.xpath('./img/@src')[0])
-		#	pdf.drawImage(ImageReader(img_path),0,0,600,600)
+	# LOGO
+	# ratio: 1.333125
+	pdf.drawImage('sakura/ginda_data/1-1.jpg', 0, 0, 400, 400*1.333125)
+	pdf.showPage()
+
+	for el in data.xpath('./body/*'):
 
 		# TEXT
-
-		if el.get('class') in ['SmallHeader,''ZeroDrop','OneDrop','FiveDrop','AlignBottom']:
+		if el.get('class') in ['SmallHeader','ZeroDrop','OneDrop','FiveDrop','AlignBottom']:
 			# break
 			if len(el.xpath('./br')) > 0: continue
 			# all text
 			text = [t for t in el.itertext()]
+			# header
+			if el.get('class') == 'SmallHeader': text = ['■'] + text + ['■']
 			# all ruby
 			ruby = el.xpath('./ruby/text()')
-
 			FURIGANA=False
 			LEN=0
 			# loop
@@ -88,6 +84,10 @@ with open(FILE,'r') as f:
 							OFFSET_LEFT-=2*16
 							# page wrap
 							if OFFSET_LEFT < 20:
+								pdf.setFont('HeiseiMin-W3', 8)
+								for i in range(0,len(str(PAGE))):
+									pdf.drawString(195+i*8, 20, str(PAGE)[i])# page num.
+								PAGE+=1
 								pdf.showPage()
 								OFFSET_TOP=560
 								OFFSET_LEFT=380
@@ -103,6 +103,10 @@ with open(FILE,'r') as f:
 						OFFSET_LEFT-=2*16
 						# page wrap
 						if OFFSET_LEFT < 20:
+							pdf.setFont('HeiseiMin-W3', 8)
+							for i in range(0,len(str(PAGE))):
+								pdf.drawString(195+i*8, 20, str(PAGE)[i])# page num.
+							PAGE+=1
 							pdf.showPage()
 							OFFSET_TOP=560
 							OFFSET_LEFT=380
@@ -121,8 +125,16 @@ with open(FILE,'r') as f:
 					pdf.drawString(OFFSET_LEFT+16, OFFSET_TOP - 8*len(text[i]) + 4*len(text[i+1]) , text[i+1])
 					OFFSET_TOP-=16*len(text[i])
 					FURIGANA=True
+pdf.setFont('HeiseiMin-W3', 8)
+for i in range(0,len(str(PAGE))):
+	pdf.drawString(195+i*8, 20, str(PAGE)[i])# page num.
 # write page
 pdf.showPage()
+# FOOT
+# ratio: 1,037667072
+pdf.drawImage('sakura/ginda_data/colo.jpg', 60, 150, 300*1.037667072, 300) 
+pdf.showPage()
+
 # write PDF
 pdf.save()
 
